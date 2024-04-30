c4089f42892042a2573be70ba4c4af3ea170bc11
Tomáš Peroutka <67205912+Pero-coder@users.noreply.github.com>
2024-04-30 18:40:03 +0200

    Update Home component to display posts dynamically

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
 import NewPostButton from "@/components/NewPostButton";
 
+import { prisma } from "@/lib/prisma";
+import Image from "next/image";
+import Link from "next/link";
+
 export default async function Home() {
+  const posts = await prisma.post.findMany()
+
   return (
     <>
-      <div>
-        <p>There are no posts yet...</p>
+      <div className="flex flex-col gap-5">
+      {posts.map(post => (
+          <Link href={`${post.authorId}/${post.id}`} key={post.id} className="flex flex-col gap-5">
+              <h1 className="text-xl font-bold">{post.title}</h1>
+              <Image alt="" src={post.imageUrl} width={16*20} height={9*20} className="rounded-md"/>
+          </Link>
+      ))}
       </div>
       <div className="fixed bottom-0 right-0 mb-10 mr-10"><NewPostButton/></div>
     </>
