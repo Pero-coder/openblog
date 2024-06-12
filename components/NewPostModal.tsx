ec3ed5e362b7ae4a352ecc2b3a1ef4abf625924c
Tomáš Peroutka <67205912+Pero-coder@users.noreply.github.com>
2024-06-12 12:37:18 +0200

    refactor: Update modal close button position and add container for children

--- components/NewPostModal.tsx
+++ components/NewPostModal.tsx
@@ -46,7 +46,7 @@ export default function CreateNewPost({ openModal, closeModal }: { openModal: bo
             ref={ref}
             onCancel={closeModal}
         >
-            <button onClick={closeModal} className="absolute top-2 right-2 bg-white rounded-full p-1">
+            <button onClick={closeModal} className="fixed top-2 right-2 bg-white rounded-full p-1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-black">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                 </svg>
