6553b9837bd21083ffc07de75daae73c266e503a
Pero-coder <67205912+Pero-coder@users.noreply.github.com>
2024-07-05 17:31:16 +0200

    comment sending state and content reset after submit

--- components/CommentComponents/Comment.tsx
+++ components/CommentComponents/Comment.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default async function Comment(
+export default function Comment(
     { comment }: { 
         comment: {
             author: {
--- components/CommentComponents/NewComment.tsx
+++ components/CommentComponents/NewComment.tsx
@@ -1,13 +1,32 @@
-import createComment from "../ServerActions/createComment"
+"use client"
 
-export default async function NewComment({ postId }: { postId: string }) {
-    const createCommentWithId = createComment.bind(null, postId)
+import createComment from "../ServerActions/createComment"
+import { useRef } from "react"
+import { useFormStatus } from "react-dom"
+
+function SubmitButton() {
+    const { pending } = useFormStatus()
 
     return (
-        <form action={createCommentWithId}>
+        <button type="submit" disabled={pending} className={`px-4 py-2 text-white rounded max-h-10 ${pending ? "bg-slate-500" : "bg-blue-500 hover:bg-blue-400"}`}>{pending ? "Sending..." : "Comment"}</button>
+    )
+}
+
+export default function NewComment({ postId }: { postId: string }) {
+    const createCommentWithId = createComment.bind(null, postId)
+    const formRef = useRef<HTMLFormElement>(null)
+
+    return (
+        <form 
+            action={ async (FormData) => {
+                await createCommentWithId(FormData)
+                formRef.current?.reset()
+            } }
+            ref={formRef}
+        >
             <div className="flex gap-3">
                 <textarea className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" name="comment" placeholder="Add a comment" required></textarea>
-                <button className="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center max-h-10" type="submit">Comment</button>
+                <SubmitButton />
             </div>
         </form>
     )
