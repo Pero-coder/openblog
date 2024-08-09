6ff943c0d865a7f7658f99c4ae1346c3925aaaf9
Pero-coder <67205912+Pero-coder@users.noreply.github.com>
2024-08-09 16:54:30 +0200

    feat: Add lazy loading for post thumbnails in Posts component

--- components/PostComponents/Posts.tsx
+++ components/PostComponents/Posts.tsx
@@ -1,10 +1,12 @@
-"use client"
+"use client";
 
 import PostsList from "./PostsList";
 import { useSession, getSession } from "next-auth/react";
-import { useState } from "react";
+import { useState, useTransition } from "react";
+import PostThumbnailSkeleton from "../Skeletons/PostThumbnailSkeleton";
 
 export default function Posts() {
+    const [isPending, startTransition] = useTransition();
     const { data: session } = useSession();
     const [followIds, setFollowIds] = useState(session?.user.following || []);
     const [followPosts, setFollowPosts] = useState(false);
@@ -12,17 +14,35 @@ export default function Posts() {
     return (
         <>
             <div className="flex flex-row gap-5 w-full m-5">
-                <button className={`w-full bg-slate-100 hover:bg-slate-50 transition-colors duration-100 rounded-md px-3 py-1 ${!followPosts && "border-b-4 border-blue-500"}`} onClick={() => setFollowPosts(false)}>
+                <button
+                    className={`w-full bg-slate-100 hover:bg-slate-50 transition-colors duration-100 rounded-md px-3 py-1 ${
+                        !followPosts && "border-b-4 border-blue-500"
+                    }`}
+                    onClick={() => setFollowPosts(false)}
+                >
                     All Posts
                 </button>
-                <button className={`w-full hover:bg-slate-50 transition-colors duration-100 bg-slate-100 rounded-md px-3 py-1 ${followPosts && "border-b-4 border-blue-500"}`} onClick={async () => {
-                    setFollowIds((await getSession())?.user.following || [])
-                    setFollowPosts(true)
-                }}>
+                <button
+                    className={`w-full hover:bg-slate-50 transition-colors duration-100 bg-slate-100 rounded-md px-3 py-1 ${
+                        followPosts && "border-b-4 border-blue-500"
+                    }`}
+                    onClick={() => {
+                        startTransition(async () => {
+                            setFollowPosts(true)
+                            setFollowIds(
+                                (await getSession())?.user.following || []
+                            )
+                        });
+                    }}
+                >
                     Following Posts
                 </button>
             </div>
-            {followPosts ? <PostsList key="all_posts" authorIds={followIds}/> : <PostsList key="follow_posts"/>}
+            {followPosts ? (
+                <PostsList key="all_posts" authorIds={followIds} />
+            ) : (
+                isPending ? <PostThumbnailSkeleton/> : <PostsList key="follow_posts" />
+            )}
         </>
     );
 }
