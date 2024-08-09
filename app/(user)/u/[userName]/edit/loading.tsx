db78629e831b74ca92d8653b8a62c63baa654367
Pero-coder <67205912+Pero-coder@users.noreply.github.com>
2024-08-09 16:22:08 +0200

    refactor: Improve loading UI for profile edit page

--- app/(user)/u/[userName]/edit/loading.tsx
+++ app/(user)/u/[userName]/edit/loading.tsx
@@ -6,15 +6,15 @@ export default function Loading() {
             <h1 className="text-xl">Edit profile</h1>
             <div className="flex flex-col gap-5">
                 <p>Image:</p>
-                <Skeleton className="w-40 h-40 rounded-full"/>
+                <Skeleton className="w-[250px] h-[250px] rounded-full"/>
             </div>
             <div className="flex flex-col gap-5">
                 <p>Name:</p>
-                <Skeleton className="w-40 h-6"/>
+                <Skeleton className="w-full h-14"/>
             </div>
             <div className="flex flex-col gap-5">
                 <p>Bio:</p>
-                <Skeleton className="w-40 h-6"/>
+                <Skeleton className="w-full h-64"/>
             </div>
         </div>
     )
